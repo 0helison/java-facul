ae15e7b33dfc9546d84c4afbd9a7d7025a51556a
Helison Pontes <helissonpontes69@gmail.com>
2024-03-19 21:27:59 -0300

    exercicio aula 7

--- api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -1,19 +1,47 @@
 package br.com.alunoonline.api.controller;
 
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
 
-    @GetMapping
+    @Autowired
+    AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Aluno aluno) {
+        alunoService.create(aluno);
+    }
+    @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
-    public String exibirNome () {
-        return "Helison";
+    public List<Aluno> findAll() {
+        return alunoService.findAll();
+
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> findById(@PathVariable Long id) {
+        return alunoService.findById(id);
+
+    }
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Aluno aluno, @PathVariable Long id) {
+        alunoService.update(id, aluno);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        alunoService.deleteById(id);
+    }
 }
